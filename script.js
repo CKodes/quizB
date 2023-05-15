1c5024d7b3c60ea562646e87ab74848d91d56af3
CKodes <78678795+CKodes@users.noreply.github.com>
2023-05-15 14:21:26 +0800

    feat: remove count api, quiz goes straight to trap

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-// Assigns visitors to gameA(with disable clicks) or gameB based on whether they're even or odd
-function websiteVisits(response) {
-  document.querySelector('#visitNo').textContent = response.value;
+// // Assigns visitors to gameA(with disable clicks) or gameB based on whether they're even or odd
+// function websiteVisits(response) {
+//   document.querySelector('#visitNo').textContent = response.value;
 
-  response.value % 5 == 0
-    ? (document.getElementById('play').href = 'gameB.html')
-    : (document.getElementById('play').href = 'gameA.html');
-}
+//   response.value % 5 == 0
+//     ? (document.getElementById('play').href = 'gameB.html')
+//     : (document.getElementById('play').href = 'gameA.html');
+// }
+(document.getElementById('play').href = 'gameA.html');
